bab3ddbef5011dd672f6ac5bc5ea8bf4e69739f5
VAHPEM <ve0912084386@gmail.com>
2026-03-24 06:48:42 +0000

    Add libyaml fuzzing project (parser, loader, emitter harnesses)

---
+++ libyaml-fuzzing/coverage_runner_load.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iterator>
+#include <vector>
+#include <stdint.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
+
+int main(int argc, char **argv) {
+    for (int i = 1; i < argc; i++) {
+        std::ifstream file(argv[i], std::ios::binary);
+        if (!file) continue;
+
+        std::vector<uint8_t> data(
+            (std::istreambuf_iterator<char>(file)),
+            std::istreambuf_iterator<char>());
+
+        LLVMFuzzerTestOneInput(data.data(), data.size());
+    }
+    return 0;
+}
